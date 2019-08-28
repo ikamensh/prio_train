be3728558300cafd2be870839ef3a37e9deb4c3c
Ilya <ikamenshchikov@gmail.com>
2019-08-28 11:43:30 +0200

    reasonable net!

--- net.py
+++ net.py
@@ -7,7 +7,7 @@ def make_discriminator_model(num_classes, color_ch = 3):
     # cnn.add(layers.BatchNormalization())
     # cnn.add(layers.LeakyReLU())
 
-    cnn.add(layers.Conv2D(32, 5, strides=2, padding='same'))
+    cnn.add(layers.Conv2D(64, 5, strides=2, padding='same'))
     cnn.add(layers.BatchNormalization())
     cnn.add(layers.LeakyReLU())
     # cnn.add(layers.Dropout(0.2))
@@ -16,14 +16,14 @@ def make_discriminator_model(num_classes, color_ch = 3):
     # cnn.add(layers.BatchNormalization())
     # cnn.add(layers.LeakyReLU())
 
-    cnn.add(layers.Conv2D(32, 5, strides=2, padding='same'))
+    cnn.add(layers.Conv2D(64, 5, strides=2, padding='same'))
     cnn.add(layers.BatchNormalization())
     cnn.add(layers.LeakyReLU())
 
     # cnn.add(layers.Dropout(0.2))
 
     cnn.add(layers.Flatten())
-    cnn.add(layers.Dense(32))
+    cnn.add(layers.Dense(64))
     cnn.add(layers.LeakyReLU())
 
     cnn.add(layers.Dense(32))
