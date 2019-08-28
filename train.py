541af23370538681d243a204d820cddeb3f2e6f3
ikkamens <ikkamens@amazon.com>
2019-08-28 11:13:57 +0200

    plain keras

--- train.py
+++ train.py
@@ -1,44 +1,48 @@
 from data import cifar_sampler, train_images, train_labels
 import tensorflow as tf
-from classifier.discriminator import Discriminator
+from net import make_discriminator_model
 cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 import time
 
 
-d = Discriminator()
-d.compile()
+d = make_discriminator_model(num_classes=10)
+d.compile(optimizer='adam', loss=cross_entropy)
 
-@tf.function
-def step():
-    x, y = cifar_sampler.sample()
 
-    with tf.GradientTape() as tape:
-        predictions = d.net(x)
-        loss = d.loss(predictions, y)
+from data import BATCH_SIZE
+batches_per_epoch = 1000
 
-    d.update(tape, loss)
 
+from ilya_ezplot import Metric, ez_plot
+
+m = Metric("images trained", "loss")
+
+def calc_n_images(epochs, batches):
+    return ( epochs * batches_per_epoch + batches ) * BATCH_SIZE
 
 print("Started training")
 for epoch in range(10):
     print("Epoch", epoch)
 
     t = time.time()
-    for i in range(1000):
-        step()
+    for i in range(batches_per_epoch):
+        x, y = cifar_sampler.sample()
+        loss = d.train_on_batch(x, y)
+        m.add_record(calc_n_images(epoch, i), loss)
 
-    d.log_metrics()
 
     print(f"Training done in {time.time() - t:.3f}")
 
     t = time.time()
 
-    y_pred = d.net.predict(train_images)
-    losses = cross_entropy.call(train_labels, y_pred)
-    cifar_sampler.update_chances(losses)
+    # y_pred = d.net.predict(train_images)
+    # losses = cross_entropy.call(train_labels, y_pred)
+    # cifar_sampler.update_chances(losses)
+    #
+    # print(f"Chances changed in {time.time() - t:.3f}")
 
-    print(f"Chances changed in {time.time() - t:.3f}")
+ez_plot(m, 'plots')
 
 
 
